27a7e4d3d66d8e639857972072557bd30c5bc57d
mfouda214 <m.fouda@robone.net>
2018-04-14 18:34:34 +0100

    Add addBarButtonTapped action

--- CoreDataDemo/TableViewController.swift
+++ CoreDataDemo/TableViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    // MARK: - Variables
+    
+    let firstNames = ["Jane", "John", "Stephen", "Stacy", "Taylor", "Alex", "Eren"]
+    let lastNames = ["White", "Black", "Fox", "Jones", "King", "McQueen", "Yeager"]
+    let ages = [25, 26, 20, 30, 27, 28, 23]
+    var people = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,29 +31,40 @@ class TableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - Add button
+    
+    @IBAction func addButtonWasTapped(_ sender: UIBarButtonItem) {
+        
+        let randomFirstName = firstNames[Int(arc4random_uniform(UInt32(firstNames.count)))]
+        let randomLastName = lastNames[Int(arc4random_uniform(UInt32(lastNames.count)))]
+        let randomAge = ages[Int(arc4random_uniform(UInt32(ages.count)))]
+        
+        people.append("\(randomFirstName) \(randomLastName) age \(randomAge)")
+        self.tableView.reloadData()
+        
+    }
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return people.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = people[(indexPath as NSIndexPath).row]
+        
         return cell
     }
-    */
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
